94b5ceec43eceb82e40abb348245576303ebecd6
IWToIT <matylitski.egor.99@gmail.com>
2024-02-03 14:14:43 +0300

    feat: "added popup window, added current date"

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from './components/Header/Header';
 import Todos from './components/Todos/Todos';
 import './scss/app.scss';
 
-
 const App: FC = () => {
   return (
     <div className="wrapper">
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,26 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import Popup from '../Popup/Popup';
 
 const Header: FC = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(true);
+
+  const [currentDate] = useState(new Date());
+  const month = new Intl.DateTimeFormat('ru', { month: 'long' }).format(currentDate);
+  const year = new Intl.DateTimeFormat('ru', { year: 'numeric' }).format(currentDate);
+  const formattedDate = `${month}${' '}${year}`;
+  const handleOpenPopup = () => {
+    setIsPopupOpen(true);
+  };
+
+  const handleClosePopup = () => {
+    setIsPopupOpen(false);
+  };
   return (
     <div className="header">
-      <h1 className="header__title">Январь 2024г.</h1>
+      <h1 className="header__title">{formattedDate}</h1>
       <div className="header__container">
         <div className="header__profile">EM</div>
-        <button className="header__addToDo">
+        <button className="btn header__addToDo" onClick={handleOpenPopup}>
           <svg fill="#000000" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg">
             <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
@@ -20,6 +34,9 @@ const Header: FC = () => {
           </svg>
         </button>
       </div>
+      {isPopupOpen && (
+        <Popup month={month} year={year} isOpen={isPopupOpen} openPopup={handleOpenPopup} closePopup={handleClosePopup} />
+      )}
     </div>
   );
 };
